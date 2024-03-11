6d75213dcc28a295a1599c882bb3913b058abb87
quiz22 <quiz22@qq.com>
2024-03-11 15:18:37 +0800

    main

--- 0308/promise/sv.js
+++ 0308/promise/sv.js
@@ -5,14 +5,15 @@ var url = require('url');
 var app = http.createServer((req,res)=>{
     var pathname = url.parse(req.url).pathname;
     console.log('链接是'+pathname);
-    fs.readFile(pathname.substr(1),()=>{
+    fs.readFile(pathname.substr(1),(err,data)=>{
         if(err){
             console.log(err);
-            res.writeHead(404,{'Content-Type':'test/html'});
+            res.writeHead(404,{'Content-Type':'text/html'});
         }else{
-            res.writeHead(200,{'Content-Type':'test/html'})
+            res.writeHead(200,{'Content-Type':'text/html'});
             res.write(data.toString());
         }
+        res.end();
     });
 });
 
