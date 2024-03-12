f1749cd0a03fc638246ce52f6ac4bf6e4372951e
quiz22 <quiz22@qq.com>
2024-03-12 08:46:30 +0800

    1

--- 0312/P61-4-1.js
+++ 0312/P61-4-1.js
@@ -4,3 +4,8 @@ var buf3 = Buffer.from('https://www.baidu.com');
 console.log(buf1);
 console.log(buf2);
 console.log(buf3);
+
+var buf4 = Buffer.alloc(256);
+var len = buf4.write('https://www.baidu.com');
+console.log('写入的字节数为：'+ len);
+console.log(buf4);
