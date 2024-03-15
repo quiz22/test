deed20c0078e904a346e2ad9bdafdd6d3e094ab2
quiz22 <quiz22@qq.com>
2024-03-15 11:02:22 +0800

    1

--- 0315/streamReadWrite.js
+++ 0315/streamReadWrite.js
@@ -6,17 +6,20 @@ var writre = fs.createWriteStream('dataWrite.txt');
 
 var data = '';
 var cnt = 0;
+
 readStream.on('data',(chunk)=>{
     data += chunk;
     cnt++;
     console.log('第'+cnt+'次得到数据块，共'+chunk.length+'字节；');
-
-    writre.write(chunk,'utf-8');
 });
+
 readStream.on('close',()=>{
+    writre.write(data,'utf-8')
     writre.end();
 });
+
     writre.on('finish',()=>{
     console.log('写入完毕');
 });
+
 console.log('程序执行完毕');
