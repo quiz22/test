90fe3c4557829fc9461f5aeaaf517a14abff3185
quiz22 <quiz22@qq.com>
2024-03-15 10:57:00 +0800

    1

--- 0315/streamReadWrite.js
+++ 0315/streamReadWrite.js
@@ -1,11 +1,20 @@
 const fs = require('fs');
 const readStream = fs.createReadStream('data.txt');
 readStream.setEncoding('utf-8');
+var writre = fs.createWriteStream('dataWrite.txt');
+
+
 var data = '';
 var cnt = 0;
 readStream.on('data',(chunk)=>{
     data += chunk;
     cnt++;
     console.log('第'+cnt+'次得到数据块，共'+chunk.length+'字节；');
+
+    writre.write(data,'utf-8');
+});
+readStream.on('close')
+    writre.on('finish',()=>{
+    console.log('写入完毕');
 });
 console.log('程序执行完毕');
