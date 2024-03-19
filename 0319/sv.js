3099557ecb9a9ddfb665d84bd1d71e063fdcd6b9
quiz22 <quiz22@qq.com>
2024-03-19 09:14:00 +0800

    1

--- 0319/sv.js
+++ 0319/sv.js
@@ -3,7 +3,7 @@ const http = require('http');
 var server = http.createServer();
 
 server.on('request',(req,res)=>{
-    
+    res.end();
 });
 server.listen(8080,()=>{
     console.log('服务器已启动，端口号为8080');
