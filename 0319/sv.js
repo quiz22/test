be0e10148eed07b0d09581e5c15dbcfcabdf60a7
quiz22 <quiz22@qq.com>
2024-03-19 09:15:20 +0800

    1

--- 0319/sv.js
+++ 0319/sv.js
@@ -3,6 +3,11 @@ const http = require('http');
 var server = http.createServer();
 
 server.on('request',(req,res)=>{
+    console.log(req.headers);
+    console.log(req.rawHeaders);
+    console.log(req.httpVersion);
+    console.log(req.method);
+    console.log(req.url);
     res.end();
 });
 server.listen(8080,()=>{
