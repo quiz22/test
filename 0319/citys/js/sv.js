e7321115c2fdc53d5af167439b677ad589340926
quiz22 <quiz22@qq.com>
2024-03-19 11:20:39 +0800

    1

--- 0319/citys/js/sv.js
+++ 0319/citys/js/sv.js
@@ -1,17 +1,14 @@
 var http = require('http');
-var fs = require('fs')
+var fs = require('fs');
 var sv = http.createServer();
 
 sv.on('request',(req,res)=>{
-    res.setHeader('Access-Control-Allow-Origin','*');
-    res.setHeader('Content-Type','text/plain; charset=utf-8');
+    res.setHeader('Access-Control-Allow-Orign','*');
+    res.setHeader('Content-Type','text/plain;charset=utf-8');
 
-    fs.readFileSync();
+    var data = fs.readFileSync('city.json');
 
     res.end();
 });
 
-sv.listen(8080,()=>{
-    console.log('服务器已启动，端口号为8080');
-    
-});
+sv.listen(8080,()=>{console.log('服务器已启动，端口号：8080')});
