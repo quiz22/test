afe9ed98329ed5c42942f892ada408c4d4d870ee
quiz22 <quiz22@qq.com>
2024-03-22 10:51:08 +0800

    1

--- 0322/urlDemo.js
+++ 0322/urlDemo.js
@@ -3,3 +3,11 @@ const url = require('url');
 var myUrl = 'https://www.ptpress.com.cn/shopping/buy/?bookid=bk001';
 var parseUrl = url.parse(myUrl);
 console.log(parseUrl);
+
+myUrl = 'https://www.ptpress.com.cn';
+var url2 = url.resolve(myUrl,'./about');
+console.log('\n'+url2);
+
+myUrl = 'https://www.ptpress.com.cn/about';
+var url3 = url.resolve(myUrl,'./shopping');
+console.log('\n'+url3);
