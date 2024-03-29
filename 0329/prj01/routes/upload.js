9c7b8e0442f56c317f5613cf05566c5025918194
quiz22 <quiz22@qq.com>
2024-03-29 10:57:04 +0800

    1

--- 0329/prj01/routes/upload.js
+++ 0329/prj01/routes/upload.js
@@ -9,6 +9,21 @@ router.get('/',function(req,res,next){
     res.sendFile('./view/index.html');
     // next();
 });
-router.post('/',)
+router.post('/',function(req,res,next){
+    var form = new multuoarty.Form();
+    form.encoding = 'utf-8';
+    from.uploadDir = './public/files';
+    from.maxFileSize = 20*1024*1024;
+    from.maxFileds = 1000;
+    from.parse(req,function(err,fileds,files){
+        var filesTemp = JSON.stringify(files,null,2);
+        if(err)
+        console.log(err);
+        else{
+            var inputFile = files.inputFile[0];
+            
+        }
+    });
+});
 
 module.exports = router;
