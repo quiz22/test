e5a8ee7ef772357def0567cbada7cca9bacf9f1b
quiz22 <quiz22@qq.com>
2024-03-29 11:18:41 +0800

    1

--- 0329/prj01/routes/upload.js
+++ 0329/prj01/routes/upload.js
@@ -3,7 +3,7 @@ var router  = express.Router();
 
 var multuoarty  = require('multiparty');
 var url = require('url');
-var fs =  reuqire('fs');
+var fs =  require('fs');
 
 router.get('/',function(req,res,next){
     res.sendFile('./view/index.html');
