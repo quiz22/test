f7918ed7be654a3b8439cfe45929719231c40add
quiz22 <quiz22@qq.com>
2024-04-02 11:15:14 +0800

    1

--- 0402/cookie/routes/index.js
+++ 0402/cookie/routes/index.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
+var {resolve} = require('path');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/checklogin',(req,res)=>{
+  let cookie = req.cookies.userName;
+  if(!cookie){
+    res.send('alert("请登录以后再操作！"); location.href = "./login.html";');
+  }else{
+    res.send('0');
+  }
+});
+
+router.post('/login',(req,res)=>{
+  
 });
 
 module.exports = router;
