35d715c87c44aeeceaf40a1d4f43dd5354ce9ad4
quiz22 <quiz22@qq.com>
2024-04-02 08:45:23 +0800

    1

--- 0402/reqDemo/routes/index.js
+++ 0402/reqDemo/routes/index.js
@@ -6,4 +6,27 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+// query,静态书写url
+router.get('/login',function(req,res){
+  let user = req.query.username;
+  let pass = req.query.password;
+  if(user === 'admin' && pass === '123456'){
+    res.send('登陆成功！');
+  }else{
+    res.send('登录失败！');
+  }
+});
+
+// 动态URL，需要使用params获取参数
+router.get('/foods/:id',(req,res)=>{
+  let id = req.params.id;
+  if(id === '1'){
+    res.render('foods1');
+  }else if(id === '2'){
+    res.render('foods2');
+  }else{
+    res.render('error');
+  }
+});
+
 module.exports = router;
