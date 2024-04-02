8affeeebb08941bccbd774aca85c5a8481ce9065
quiz22 <quiz22@qq.com>
2024-04-02 09:27:57 +0800

    1

--- 0402/reqDemo/routes/index.js
+++ 0402/reqDemo/routes/index.js
@@ -1,20 +1,13 @@
 var express = require('express');
 var router = express.Router();
+var bodyParser = require('body-parser');
 
+var pathViews = __dirname.substring(0,__dirname.lastIndexOf('\\')) + '/views/';
+ 
+  
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
-});
-
-// query,静态书写url
-router.get('/login',function(req,res){
-  let user = req.query.username;
-  let pass = req.query.password;
-  if(user === 'admin' && pass === '123456'){
-    res.send('登陆成功！');
-  }else{
-    res.send('登录失败！');
-  }
 });
 
 // 动态URL，需要使用params获取参数
@@ -29,4 +22,25 @@ router.get('/foods/:id',(req,res)=>{
   }
 });
 
+// query,静态书写url
+router.get('/login',function(req,res){
+  // let user = req.query.username;
+  // let pass = req.query.password;
+  // if(user === 'admin' && pass === '123456'){
+  //   res.send('登陆成功！');
+  // }else{
+  //   res.send('登录失败！');
+  // }
+  res.sendFile(pathViews + 'login.html');
+});
+
+router.post('/login', (req, res) => {  
+  var user = req.body.username;  
+  var pass = req.body.password;  
+  if (user === 'admin' && pass === '123456') {  
+    res.send('<h1>登录成功！</h1>');  
+  } else {  
+    res.send('<h1>登录失败！</h1>');  
+  }  
+}); 
 module.exports = router;
