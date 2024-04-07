bb9ca24e5b4ee5dc39b0deb90c97ee47f9914ddb
quiz22 <quiz22@qq.com>
2024-04-07 11:19:43 +0800

    1

--- 0407/session/routes/index.js
+++ 0407/session/routes/index.js
@@ -2,9 +2,28 @@ var express = require('express');
 var session = require('express-session');
 var router = express.Router();
 
+router.use(session({
+  secret: 'keyBoard cat',
+  resave: false,
+  saveUninitialized: true,
+  cookie: ('name','value',{
+    maxAge: 1000
+  })
+}));
+
+router.use('/login',(req,res)=>{
+  req.session.userinfo = 'Harrison';
+  res.send('登录成功')
+});
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  if(req.session.userinfo){
+    res.send('Hello' + req.session.userinfo + ',wellcome')
+  }else{
+    res.send('未登录！')
+  }
+  // res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
