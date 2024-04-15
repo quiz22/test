e5432a22ae40b0e4c69d588f1f411e6c78d75f20
quiz22 <quiz22@qq.com>
2024-04-15 14:42:50 +0800

    1

--- 0409/stumanager/public/javascripts/stumanager.js
+++ 0409/stumanager/public/javascripts/stumanager.js
@@ -1,3 +1,8 @@
 $('#btnAdd').click(function(){
-    alert('添加学生信息')
+    // alert('添加学生信息')
+    $.ajax({
+        url: '/student/add',
+        method: 'post',
+        data:   $('$frmAppend').serialize()
+    })
 });
